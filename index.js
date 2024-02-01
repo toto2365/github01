7b3bca6bd6599917b594daeb1ab453e04701bc82
unknown <toto126589@gmail.com>
2024-02-01 22:53:57 +0100

    script

---
+++ index.js
@@ -0,0 +1,10 @@
+// let age = 50;
+// let price = 10.99;
+// let gpa = 2.1;
+
+let firstName = "Bro";
+// console.log('you are ${age} years old');
+// console.log('The price is $${price}');
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
